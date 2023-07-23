cc88ac1fa8a5cc17dae47e0ff3ac7fa2a02c2743
The Friendly Giant <139039265+TheFriendlyGiant1@users.noreply.github.com>
2023-07-23 21:31:09 +0100

    Create load-tests.js

---
+++ load-tests.js
@@ -0,0 +1,56 @@
+function loadTests() {
+    /**
+     * This function loads and runs multiple tests simultaneously.
+     * 
+     * Returns:
+     * Promise: A promise that resolves when all tests have completed.
+     */
+    
+    // Create an array to store the promises for each test
+    const testPromises = [];
+    
+    // Test 1: Load and run test 1
+    const test1Promise = new Promise((resolve, reject) => {
+        // Load and run test 1
+        // Replace the code below with the actual code for test 1
+        setTimeout(() => {
+            console.log("Test 1 completed");
+            resolve();
+        }, 2000);
+    });
+    testPromises.push(test1Promise);
+    
+    // Test 2: Load and run test 2
+    const test2Promise = new Promise((resolve, reject) => {
+        // Load and run test 2
+        // Replace the code below with the actual code for test 2
+        setTimeout(() => {
+            console.log("Test 2 completed");
+            resolve();
+        }, 3000);
+    });
+    testPromises.push(test2Promise);
+    
+    // Test 3: Load and run test 3
+    const test3Promise = new Promise((resolve, reject) => {
+        // Load and run test 3
+        // Replace the code below with the actual code for test 3
+        setTimeout(() => {
+            console.log("Test 3 completed");
+            resolve();
+        }, 2500);
+    });
+    testPromises.push(test3Promise);
+    
+    // Return a promise that resolves when all tests have completed
+    return Promise.all(testPromises);
+}
+
+// Example usage:
+loadTests()
+    .then(() => {
+        console.log("All tests completed");
+    })
+    .catch((error) => {
+        console.error("Error occurred during test execution:", error);
+    });
